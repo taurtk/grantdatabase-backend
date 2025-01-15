e6c87dfd30fcdf2a77c5d5286c2266f2eab8e312
Taufique Khan <taurtk@gmail.com>
2025-01-15 21:19:24 +0530

    second commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,7 +13,12 @@ exports.registerUser = async (req, res) => {
 
     try {
         await user.save();
-        res.status(201).json({ message: 'User registered successfully' });
+
+        // Generate a JWT token
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        // Send the token in the response
+        res.status(201).json({ message: 'User registered successfully', token });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
